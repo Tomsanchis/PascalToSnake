8a6ca0bb6809736d7db384bcbff5a99728aa0cd6
Tomsanchis <92373414+Tomsanchis@users.noreply.github.com>
2022-03-10 15:41:46 +0100

    pascal mocha test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
@@ -6,3 +7,25 @@ describe('Array', function () {
     });
   });
 });
+
+
+describe('pascaltosnake', () => {
+  it('should return TestController', () => {
+    assert.equal(pascaltosnake("TestCcontroller"), "test_controller");
+  });
+  it('should return MovieAndBook', () => {
+    assert.equal(pascaltosnake("MovieAndBook"), "movie_and_book");
+  });
+  it('should return App7Test', () => {
+    assert.equal(pascaltosnake("App7Test"), "app7_test");
+  });
+  it('should return play-movie', () => {
+    assert.equal(pascaltosnake("play-movie"), "play-movie");
+  });
+  it('should return Home', () => {
+    assert.equal(pascaltosnake("Home"), "home");
+  });
+  it('should return \"1\"', () => {
+    assert.equal(pascaltosnake("1"), "\"1\"");
+  });
+})
