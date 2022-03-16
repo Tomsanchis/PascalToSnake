b288086fb64f3b6448237f801df4cb833ae4f505
Tomsanchis <92373414+Tomsanchis@users.noreply.github.com>
2022-03-16 09:44:46 +0100

    pascal to snake finish

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 export const pascaltosnake = (entry) => {
         let news = "";
         if (!isNaN(entry)) {
-            return news =+ entry.toString();
+            return news += entry.toString();
         }
+        for(let i = 0; i < entry.length; i++){
+            if  (entry[i].match(/[A-Z]/) && i != 0) {
+                news += "_"
+                news += entry[i];
+            } else  {
+                news += entry[i];
+            }
+        }
+        return news.toLowerCase();
 }
